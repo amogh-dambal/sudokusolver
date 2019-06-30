75363fa4645d155136a3a8d435199c8b612841fb
Amogh Dambal <amdam2013@gmail.com>
2019-06-30 18:22:25 -0500

    added user file input functionality and minor error handling
    - added the ability to specify sudoku input file through keyboard input or as command line argument
    - added null check for filename
    -

--- sudoku/src/sudoku/Main.java
+++ sudoku/src/sudoku/Main.java
@@ -1,13 +1,39 @@
 package sudoku;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
 	// launch solver with sudoku filename
+	// command line argument
 	public static void main(String[] args) {
-		// launch solver
 		
-		Solver s = new Solver("resources/test.txt");
+		String filename = new String();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		if (args.length > 0) {
+			filename = args[0];
+		}
+		else {
+			System.out.print("enter sudoku filename: ");
+			try {
+				filename = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+				filename = null;
+			}
+		}
+		
+		Solver s;
+		if (filename != null) {
+			s = new Solver(filename);
+		}
+		else {
+			System.err.println("Error: File is null");
+			return;
+		}
 		
 		s.solve();
 		System.out.println(s.getGrid());
